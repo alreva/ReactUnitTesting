5394b17147f5941944be33c664df9a3a931ddfcf
alreva@gmail.com <alreva@gmail.com>
2017-07-18 19:53:20 +0300

    Added module unit tests

---
+++ tests/routes/Categories/modules/categories.spec.js
@@ -0,0 +1,54 @@
+import {
+    CATEGORIES_ALL_SUCCESS,
+    CATEGORIES_DELETE,
+    default as categoriesReducer
+} from 'routes/Categories/modules/categories'
+
+describe("Categories > categories module", () => {
+
+    describe("Reducer", () => {
+
+        it("Should be a function", () => {
+            expect(categoriesReducer).to.be.a('function')
+        })
+
+        describe(`Action ${CATEGORIES_ALL_SUCCESS}`, () => {
+            
+            it("Load Categories", () => {
+                categoriesReducer(
+                    [],
+                    {
+                        type: CATEGORIES_ALL_SUCCESS,
+                        categories: ["1", "2"]
+                    }
+                ).should.deep.equal(["1", "2"])
+            })
+        })
+
+        describe(`Action ${CATEGORIES_DELETE}`, () => {
+
+            it("Delete category", () => {
+                categoriesReducer(
+                    [{id: '1'}, {id: '2'}],
+                    {
+                        type: CATEGORIES_DELETE,
+                        id: '2'
+                    }
+                ).should.deep.equal([{id: '1'}])
+            })
+
+            it("Delete missing category = no change", () => {
+                categoriesReducer(
+                    [{id: '1'}, {id: '2'}],
+                    {
+                        type: CATEGORIES_DELETE,
+                        id: '3'
+                    }
+                ).should.deep.equal([{id: '1'}, {id: '2'}])
+            })
+
+        })
+
+    })
+
+})
